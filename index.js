a21f2b94bc5b54809b873a0fc9797dc80038d6df
Alex <agordon1123@gmail.com>
2019-09-16 13:42:01 -0400

    created GET /users

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ server.post('/api/login', (req, res) => {
     });
 });
 
-server.get('/api/users', (req, res) => {
+server.get('/api/users', credChecker, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
@@ -65,3 +65,23 @@ server.get('/api/hash', (req, res) => {
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
+
+// Middleware
+
+function credChecker(req, res, next) {
+  const { username, password } = req.headers;
+  if (username && password) {
+    Users.findBy({ username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next();
+        } else {
+          res.status(401).json({ message: 'Invalid credentials' });
+        }
+      })
+      .catch(err => res.status(500).json({ message: 'Unexpected error' }));
+  } else {
+    res.status(400).json({ message: 'No credentials provided' });
+  };
+};
